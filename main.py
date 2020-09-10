ec9e55ef2d33d5cd882ba42032a66f9f83ea6c65
criscujia <cjcujia@uniguajira.edu.co>
2020-09-09 20:53:11 -0500

    agregue el Menu

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("hola chicos XD")
+def main():
+    while True:        
+
+        print ("Selecciona una opción")
+        print ("\t1 - Insertar")
+        print ("\t2 - Mostrar")
+        print ("\t3 - Buscar")
+        print ("\t4 - Eliminar")
+        print ("\t5 - Salir")
+
+        opcionMenu = input("inserta un numero valor >> ")
+
+        if opcionMenu=="1":  
+            print ("")
+        elif opcionMenu=="2":
+            print ("")
+        elif opcionMenu=="3":
+            print ("")
+        elif opcionMenu=="4":
+            print ("")
+        elif opcionMenu=="5":
+            print ("")
+            break
+        else:
+            print ("")
+            input("La opcion ingresada no es valida... \npulse una tecla para continuar")
+
+
+vec = []
+diccionario = {}
+main()
